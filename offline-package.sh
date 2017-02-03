93ee494c595887c168972592a8a9786b79c9fc6a
Jonathan Meyer <jon@gisjedi.com>
2017-02-03 11:32:50 -0500

    Update to pull extra packages not referenced by group.json

--- offline-package.sh
+++ offline-package.sh
@@ -5,12 +5,15 @@ cd "$(dirname "$0")"
 mkdir delivery 
 cd delivery
 
-for IMAGE in `grep \"image\" ../group.json | awk '{print $2}' | cut -d\" -f2`;
+EXTRA="wrouesnel/postgres_exporter:v0.1.1 prom/haproxy-exporter:v0.7.1"
+for IMAGE in `grep \"image\" ../group.json | awk '{print $2}' | cut -d\" -f2` $EXTRA;
 do
-        FILE_IMAGE_NAME=`echo $IMAGE | cut -d\" -f2 | sed 's^[/:]^-^g'`
+        FILE_IMAGE_NAME=`echo $IMAGE | cut -d\" -f2 | sed 's^[/:]^-^g'`.tar.gz
 	docker pull $IMAGE
-        docker save $IMAGE | gzip > $FILE_IMAGE_NAME.tar.gz
+        echo Saving $FILE_IMAGE_NAME...
+        docker save $IMAGE | gzip > $FILE_IMAGE_NAME
 done
+
 for ZIP in `grep uri ../group.json | awk '{print $2}' | cut -d\" -f 2`;
 do
 	wget -k $ZIP
